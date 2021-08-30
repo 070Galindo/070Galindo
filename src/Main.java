031cda702cc3b5d255be7a8a4b532eec09d92f4c
070Galindo <74683233+070Galindo@users.noreply.github.com>
2021-08-30 13:31:13 -0700

    Added startup class and Rectangle class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import AppCode.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        App app = new App();
+        app.start();
+    }
+}
